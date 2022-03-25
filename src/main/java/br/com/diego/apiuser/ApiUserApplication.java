18a0779d919cbd10ad2cf49a4fd200df39595514
diegocardosodev <diegocardososacramento@gmail.com>
2022-03-25 15:27:59 -0300

    project init

---
+++ src/main/java/br/com/diego/apiuser/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.apiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diego/apiuser/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.apiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
