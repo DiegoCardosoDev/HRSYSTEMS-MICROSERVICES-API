13748b15e3de9ebf6692208da81df358162aea17
diegocardosodev <diegocardososacramento@gmail.com>
2022-03-31 21:06:18 -0300

    eureka implements

--- user-api/src/main/java/br/com/diego/userapi/UserApiApplication.java
+++ user-api/src/main/java/br/com/diego/userapi/UserApiApplication.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 import java.util.List;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApiApplication implements CommandLineRunner {
 
--- user-api/src/main/java/br/com/diego/userapi/services/UserServiceImpl.java
+++ user-api/src/main/java/br/com/diego/userapi/services/UserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class UserServiceimpl implements UserService{
+public class UserServiceImpl implements UserService{
 
     private final UserRepo userRepo;
 
